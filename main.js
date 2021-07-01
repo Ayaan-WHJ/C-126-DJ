1b13a3cb36f7b3b5093284a7552a29dec77b804e
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-07-01 16:47:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX = 0 ;
 leftWristY = 0 ;   
 rightWristX = 0 ;
 rightWristY = 0 ; 
+scoreLeftWrist = 0 ;
 function preload()
 {
 song = loadSound('music.mp3');
@@ -17,10 +18,34 @@ video.hide();
 poseNet = ml5.poseNet(video,ModelLoaded);
 poseNet.on("pose",gotPoses);
 }
-
+function gotPoses(results)
+{
+if (results.length > 0)
+ { 
+    console.log(results);
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
+    console.log("Score Left Wrist = " + scoreLeftWrist);
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("Right Wrist Y = " + rightWristY + " Right Wrist X = " + rightWristX);
+}
+}
 function draw()
 {
     image(video,0,0,600,500);
+    fill("#0000FF");
+    stroke("#0000FF");
+    if(scoreLeftWrist > 0.2){
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    leftWristY_divided_by_1000 = remove_decimals/1000;
+    volume = leftWristY_divided_by_1000*2;
+    document.getElementById("volume").innerHTML = "Volume = " + volume;
+    song.setVolume(volume);}
 }
 function play()
 {
@@ -31,17 +56,4 @@ function play()
 function ModelLoaded()
 {
     console.log("Pose Net is initialized");
-}
-function gotPoses(results)
-{
-if (results.length > 0)
- { 
-    console.log(results);
-    leftWristX = results[0].pose.leftWrist.x;
-    leftWristY = results[0].pose.leftWrist.y;
-    console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
-    rightWristX = results[0].pose.rightWrist.x;
-    rightWristY = results[0].pose.rightWrist.y;
-    console.log("Right Wrist Y = " + rightWristY + " Right Wrist X = " + rightWristX);
-}
 }
